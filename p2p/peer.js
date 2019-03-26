f99dc6ce788fd3f619055e06d4aea1f2d335c709
Artback <artback@openmailbox.org>
2019-03-26 17:20:46 +0100

    Add a network speed function to calculate the average speed of the downloaded packages

--- p2p/peer.js
+++ p2p/peer.js
@@ -1,10 +1,9 @@
 const hypercore = require("hypercore");
+const now = require("performance-now");
 // lets you to find other hyper* peers
 const hyperdiscovery = require("hyperdiscovery");
 // lets you to write to memory as if you are writing to a file
 const ram = require("random-access-memory");
-//const fs = require('fs');
-//const source = fs.createWriteStream('./write.mkv');
 
 // pass in the key that's printed from swarm-write.js
 const remote = process.argv[2];
@@ -22,7 +21,7 @@ feed.on("ready", function() {
     swarm = hyperdiscovery(feed, {live: true});
     // triggered when a peer connects
     swarm.on("connection", function(peer, type) {
-        console.log("Connected");
+        //console.log("Connected");
     })
 })
 
@@ -30,6 +29,18 @@ feed.on("ready", function() {
 // 'start: 0' makes the stream start at the beginning of the log
 // 'live: true' keeps the stream open after the last bit of data has been read, yielding more data as it comes in
 const stream = feed.createReadStream({start: 0, live: true});
+const averageNetworkSpeed = (function () {
+    let totalSizeInKbit = 0;
+    let totalTimeInSeconds = 0;
+    return function (start, sizeofBlockInBytes, now) {
+        totalTimeInSeconds += ((now-start).toFixed(3)/1000);
+        totalSizeInKbit += (sizeofBlockInBytes/1000)*8;
+        return totalSizeInKbit/totalTimeInSeconds;
+    }
+})();
+var t0 = now();
 stream.on("data", function(data) {
+    console.log(averageNetworkSpeed(t0,data.length,now()));
+    t0 = now();
 });
 
